17a7262d9766e503c2b3e7f471eac1267e745464
Abdalla Diaai <56742529+abdalla-diaai@users.noreply.github.com>
2024-02-17 11:36:28 +0000

    added a test link to license icons.

--- starter/utils/generateMarkdown.js
+++ starter/utils/generateMarkdown.js
@@ -1,6 +1,14 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}`;
+  return `# ${data.title} [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  
+  ## Description:\n ${data.description}
+  ## Installation:\n ${data.installation}
+  ## Usage:\n ${data.usage}
+  ## License:\n ${data.license}
+  ## Contribution:\n ${data.contribution}
+  ## Questions:\n ${data.questions}
+  `;
 }
 
 module.exports = generateMarkdown;
